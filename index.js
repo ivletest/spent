af85f83f8ea4c62478c74a372872821cb8235b3a
ivan <itr@mca.dev>
2020-01-27 17:29:22 +0100

    moved index.js from dist to root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const http = require("http");
-const app = require("../../app");
+const app = require("./app");
 
 const port = Number(process.env.APP_PORT);
 app.set("port", port);
