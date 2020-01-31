6566771e36a9c4a07bc5241105f0ee02bec42e60
ivan <itr@mca.dev>
2020-01-31 13:38:29 +0100

    create user basic implementation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,22 +5,13 @@ require("../extensions/string.extensions");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const userRepository = require("../repository/user.repository");
 
 const users = express.Router();
 users.use(cors());
 
-const db = require("../models/index");
-
 // REGISTER
 users.post("/register", async (req, res, next) => {
-    // Validate request
-    let userData = {};
-    try {
-
-    } catch (err) {
-        next(err);
-    }
-
     try {
         const username = req.body.username.validateUsername()
         const password = req.body.password.validateAndHashPassword();
@@ -33,45 +24,9 @@ users.post("/register", async (req, res, next) => {
             email: email
         }
 
-        try {
-            const result = await sequelize.transaction(async (t) => {
+        let result = await userRepository.createUser(userData);
+        res.status(result.status).send(result.data);
 
-            });
-        } catch(err) {
-            next(err);
-        }
-        await db.Email.findOrCreate({
-            where: { address: userData.email },
-            defaults: {
-                name: userData.username,
-                passwordHash: userData.passwordHash,
-                passwordSalt: userData.passwordSalt,
-                role: "user",
-                email: {
-                    address: userData.email
-                },
-            }
-        }).spread((result, created) => {
-            console.log(result);
-        });
-        // const result = await db.User.create({
-        //     name: userData.username,
-        //     passwordHash: userData.passwordHash,
-        //     passwordSalt: userData.passwordSalt,
-        //     role: "user",
-        //     email: {
-        //         address: userData.email
-        //     }
-        // }, {
-        //     include: [{
-        //         model: db.Email,
-        //         as: "email"
-        //     }]
-        // });
-
-        // if (result) {
-        //     res.status(400).send(result);
-        // }
     } catch (err) {
         next(err);
     }
