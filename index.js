d1e28ab39ccb950a029d6cf1ed5c3a9c16a71780
ivletest <karakan@protonmail.com>
2020-02-02 16:35:42 +0100

    fixed untracked files

--- index.js
+++
@@ -1,6 +0,0 @@
-const { server, port } = require("./server/server");
-const router = require("./server/router");
-
-server.listen(port, (router) => {
-  console.log(`server listens to port: ${port}`);
-});
