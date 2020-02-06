9ac0af5b134d53ff790c9a351e1979efc07164f9
ivan <itr@mca.dev>
2020-02-06 16:19:26 +0100

    Merge from master

--- server/seeds/20200202191534-accounts.js
+++ server/seeds/20200202191534-accounts.js
@@ -18,7 +18,7 @@ module.exports = {
             currency: "mkd",
             created_at: new Date(),
             updated_at: new Date(),
-            parent_account_uid: "ca5cb211-494d-4174-853f-e70a148c2b33"
+            parent_account_uid: "60ce244f-2397-477d-a61c-8df5a43bb251"
         }]);
     },
 
