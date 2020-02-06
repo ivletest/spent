8b38604e125892802322f2c80eeaaed776800b98
ivan <itr@mca.dev>
2020-02-06 13:18:38 +0100

    Created auth unit tests

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -1,27 +1,65 @@
-const restify = require("restify-clients");
+require("../index");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
-const client = restify.createJsonClient({
-    version: "*",
-    url: `${process.env.HOST}:${process.env.PORT}`
-})
+const host = `${process.env.HOST}:${process.env.PORT}`;
 
-describe("Register user test", () => {
+chai.use(chaiHttp);
+chai.should();
+
+// SUCESS TESTS
+describe("/POST auth/register", () => {
+    const userEmail = `test${Math.floor(Math.random() * 89999) + 10000}@email.com`;
+    const userPassword = "Testpass0";
+    let authToken = "";
+
+    // Create random user
     it("Should return status 201", (done) => {
-        client.post("/auth/register", {
-            uername: "test user",
-            email: "test@email.com",
-            password: "Testpass0"
-        },
-            (error, request, response, data) => {
-                if (error) {
-                    throw new Error(error);
-                }
 
-                if (data.code !== 201) {
-                    throw new Error("Invlid response code.");
-                }
+        const registerUserRequest = {
+            username: "test user",
+            email: userEmail,
+            password: userPassword
+        }
 
+        chai.request(host)
+            .post("/auth/register")
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(registerUserRequest)
+            .end((error, response) => {
+                response.should.have.status(201);
+                response.body.should.be.a("object");
+                response.body.should.have.property("username");
+                response.body.should.have.property("email");
                 done();
             });
     });
+
+    //Login user
+    it("Should return status 200", (done) => {
+
+        const loginUserRequest = {
+            email: userEmail,
+            password: userPassword
+        }
+
+        chai.request(host)
+            .post("/auth/login")
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(loginUserRequest)
+            .end((error, response) => {
+                response.should.have.status(200);
+                response.header.should.have.property("auth_token");
+                response.body.should.be.a("object");
+                response.body.should.have.property("username");
+                response.body.should.have.property("email");
+
+                authToken = response.header.auth_token;
+                done();
+            });
+    });
+    
+    //Logout user
+
+    //Remove user
 });
