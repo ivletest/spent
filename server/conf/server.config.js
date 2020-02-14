fa843c5cfb3e07a9d7521404e1936611facb4ff0
ivan <itr@mca.dev>
2020-02-14 16:56:54 +0100

    implemented basic proxy for api

--- server/conf/server.config.js
+++ server/conf/server.config.js
@@ -1,14 +1,18 @@
 module.exports = {
     api: {
-        host: process.env.API_HOST || "127.0.0.1",
+        host: process.env.API_HOST || "127.0.2.1",
         port: process.env.API_PORT || 3000
     },
     static: {
-        host: process.env.STATIC_HOST || "127.0.0.1",
+        host: process.env.STATIC_HOST || "127.0.2.1",
         port: process.env.STATIC_PORT || 8008
     },
     smtp: {
-        host: process.env.SMTP_HOST || "127.0.0.1",
+        host: process.env.SMTP_HOST || "127.0.2.1",
         port: process.env.SMTP_PORT || 2525
+    },
+    proxy: {
+        host: process.env.PROXY_HOST || "127.0.0.1",
+        port: process.env.PROXY_PORT || 80
     }
 }
--- server/proxy/proxy.server.js
+++ server/proxy/proxy.server.js
@@ -1,18 +1,31 @@
 require("dotenv").config();
+const config = require("../conf/server.config");
 const http = require("http");
 const httpProxy = require("http-proxy");
+const httpProxyRules = require("http-proxy-rules");
+
+const proxyRules = new httpProxyRules({
+    rules: {
+        '.*/api': `http://${config.api.host}:${config.api.port}`
+    },
+    default: ''
+});
 
 const proxy = httpProxy.createProxyServer();
 
-
-
 const server = http.createServer((request, response) => {
-    proxy.web(request, response, { 
-        target: {
-            host: process.env.API_HOST,
-            port: process.env.API_PORT
-        }
-    });
+    var target = proxyRules.match(request);
+
+    if (target) {
+      return proxy.web(request, response, {
+        target: target
+      });
+    }
+
+    response.statusCode = 407;
+    return response.end("Nope.");
 });
+
+server.listen(config.proxy.port, config.proxy.host);
 
 module.exports = server;
